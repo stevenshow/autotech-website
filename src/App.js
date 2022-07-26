8181f9f43c72f262b34e5aa08474869e66a3f92c
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-07-26 11:58:49 -0600

    working state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './styles/App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Autotech Steve</header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">Autotech Steve</div>
+    </>
+  );
+};
+
+export default Header;
