ad90b3d1c8ed7d22eafe3c64a2043fbb6a4285e2
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-07-26 16:45:58 -0600

    created header with title and 3 links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './styles/App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './styles/App.scss';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/services" element={<></>} />
+          <Route path="/contact" element={<></>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
-import '../styles/Header.css';
+import { Link } from 'react-router-dom';
+import '../styles/Header.scss';
 
 const Header = () => {
   return (
     <>
-      <div className="header">Autotech Steve</div>
+      <header className="header">Autotech Steve</header>
+      <nav className="nav-links">
+        <Link id="home" className="link" to="/">
+          Home
+        </Link>
+        <Link id="home" className="link" to="/services">
+          Services
+        </Link>
+        <Link id="home" className="link" to="/contact">
+          Contact Me
+        </Link>
+      </nav>
     </>
   );
 };
