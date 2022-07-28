c6b96eff6ba2e6b70889be057b8e85b8009f1583
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-07-27 21:00:22 -0600

    restructure files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
-import App from './App';
+import App from './pages/App';
 import reportWebVitals from './utils/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './styles/App.scss';
-import Header from './components/Header';
+import '../styles/App.scss';
+import Header from '../components/Header';
 
 function App() {
   return (
