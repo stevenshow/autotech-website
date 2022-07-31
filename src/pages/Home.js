4292bed1cd25f33279e696d64fc6a89671e68f1c
stevenshow <steven.schoebinger@gmail.com>
2022-07-31 01:57:41 -0600

    extracted card component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,4 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
-import Typography from '@mui/material/Typography';
+import CardWrapper from '../components/CardWrapper';
 import '../styles/Home.scss';
 import kickersub from '../images/KickerSub.jpg';
 import mecp from '../images/MECP_logo_250x100_L-2.png';
@@ -11,63 +7,15 @@ const Home = () => {
   return (
     <>
       <div className="container">
-        <Card>
-          <CardActionArea>
-            <CardMedia component="img" minHeight="300" image={mecp} alt="MECP logo" />
-            <CardContent>
-              <Typography gutterBottom variant="h4" component="div">
-                MECP
-              </Typography>
-              <Typography variant="body1" color="text.primary">
-                This training has given me the confidence to tackle almost any vehicle install out
-                there. It also gives you the confidence that I will perform industry standard
-                install practices while in your vehicle.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Card>
-          <CardActionArea>
-            <CardMedia component="img" height="250" image={kickersub} alt="kicker sub" />
-            <CardContent>
-              <Typography gutterBottom variant="h4" component="div">
-                Kicker
-              </Typography>
-              <Typography variant="body1" color="text.primary">
-                After installing for more than 5 years I have come to love Kicker for those wanting
-                to enter the car audio scene, as well as those who have been around for a while.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Card>
-          <CardActionArea>
-            <CardMedia component="img" height="250" image={kickersub} alt="kicker sub" />
-            <CardContent>
-              <Typography gutterBottom variant="h4" component="div">
-                Kicker
-              </Typography>
-              <Typography variant="body1" color="text.primary">
-                After installing for more than 5 years I have come to love Kicker for those wanting
-                to enter the car audio scene, as well as those who have been around for a while.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Card>
-          <CardActionArea>
-            <CardMedia component="img" height="250" image={kickersub} alt="kicker sub" />
-            <CardContent>
-              <Typography gutterBottom variant="h4" component="div">
-                Kicker
-              </Typography>
-              <Typography variant="body1" color="text.primary">
-                After installing for more than 5 years I have come to love Kicker for those wanting
-                to enter the car audio scene, as well as those who have been around for a while.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+        <CardWrapper title="MECP" image={mecp}>
+          This training has given me the confidence to tackle almost any vehicle install out there.
+          It also gives you the confidence that I will perform industry standard install practices
+          while in your vehicle.
+        </CardWrapper>
+        <CardWrapper title="Kicker" image={kickersub}>
+          After installing for more than 5 years I have come to love Kicker for those wanting to
+          enter the car audio scene, as well as those who have been around for a while.
+        </CardWrapper>
       </div>
     </>
   );
