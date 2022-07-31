91b87fbf6a1d1d8f1ef7da0ec3232ae2ea27a6f9
stevenshow <steven.schoebinger@gmail.com>
2022-07-30 23:37:43 -0600

    add mecp card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import '../styles/Home.scss';
 import kickersub from '../images/KickerSub.jpg';
+import mecp from '../images/MECP_logo_250x100_L-2.png';
 
 const Home = () => {
   return (
@@ -23,7 +24,7 @@ const Home = () => {
         </Card>
         <div className="test">
           <Card sx={{ backgroundColor: 'gray' }}>
-            <CardMedia component="img" height="250" image={kickersub} alt="kicker sub" />
+            <CardMedia component="img" height="300" image={mecp} alt="MECP logo" />
             <CardContent>
               <Typography gutterBottom variant="h4" component="div">
                 Kicker
