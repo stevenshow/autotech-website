34a2b943567e004d613f7c4886eff0afc4867e4f
stevenshow <steven.schoebinger@gmail.com>
2022-07-31 14:09:46 -0600

    changed layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,14 +13,14 @@ const Home = () => {
           It also gives you the confidence that I will perform industry standard install practices
           while in your vehicle.
         </CardWrapper>
-        <CardWrapper title="Kicker" image={kickervt}>
-          After installing for more than 5 years I have come to love Kicker for those wanting to
-          enter the car audio scene, as well as those who have been around for a while.
-        </CardWrapper>
         <CardWrapper title="Audiophile" image={audiophile}>
           I have always been intrigued by audio and the amazing things that can be created and
           experienced. This naturally drew me into car audio due to the simple fact that I spent a
           majority of my time listening to music on my commute to work.
+        </CardWrapper>
+        <CardWrapper title="Kicker" image={kickervt}>
+          After installing for more than 5 years I have come to love Kicker for those wanting to
+          enter the car audio scene, as well as those who have been around for a while.
         </CardWrapper>
       </div>
     </>
