a27c3bd6d9d963474fbb7b592e836873ff4c7139
stevenshow <steven.schoebinger@gmail.com>
2022-07-31 01:27:22 -0600

    add card action for future reading about each card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import '../styles/Home.scss';
 import kickersub from '../images/KickerSub.jpg';
@@ -11,52 +12,61 @@ const Home = () => {
     <>
       <div className="container">
         <Card sx={{ backgroundColor: 'gray' }}>
-          <CardMedia component="img" height="250" image={kickersub} alt="kicker sub" />
-          <CardContent>
-            <Typography gutterBottom variant="h4" component="div">
-              Kicker
-            </Typography>
-            <Typography variant="body1" color="text.primary">
-              After installing for more than 5 years I have come to love Kicker for those wanting to
-              enter the car audio scene, as well as those who have been around for a while.
-            </Typography>
-          </CardContent>
+          <CardActionArea>
+            <CardMedia component="img" height="250" image={kickersub} alt="kicker sub" />
+            <CardContent>
+              <Typography gutterBottom variant="h4" component="div">
+                Kicker
+              </Typography>
+              <Typography variant="body1" color="text.primary">
+                After installing for more than 5 years I have come to love Kicker for those wanting
+                to enter the car audio scene, as well as those who have been around for a while.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
         </Card>
         <Card sx={{ backgroundColor: 'gray' }}>
-          <CardMedia component="img" height="300" image={mecp} alt="MECP logo" />
-          <CardContent>
-            <Typography gutterBottom variant="h4" component="div">
-              Kicker
-            </Typography>
-            <Typography variant="body1" color="text.primary">
-              After installing for more than 5 years I have come to love Kicker for those wanting to
-              enter the car audio scene, as well as those who have been around for a while.
-            </Typography>
-          </CardContent>
+          <CardActionArea>
+            <CardMedia component="img" minHeight="300" image={mecp} alt="MECP logo" />
+            <CardContent>
+              <Typography gutterBottom variant="h4" component="div">
+                MECP
+              </Typography>
+              <Typography variant="body1" color="text.primary">
+                This training has given me the confidence to tackle almost any vehicle install out
+                there. It also gives you the confidence that I will perform industry standard
+                install practices while in your vehicle.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
         </Card>
         <Card sx={{ backgroundColor: 'gray' }}>
-          <CardMedia component="img" height="250" image={kickersub} alt="kicker sub" />
-          <CardContent>
-            <Typography gutterBottom variant="h4" component="div">
-              Kicker
-            </Typography>
-            <Typography variant="body1" color="text.primary">
-              After installing for more than 5 years I have come to love Kicker for those wanting to
-              enter the car audio scene, as well as those who have been around for a while.
-            </Typography>
-          </CardContent>
+          <CardActionArea>
+            <CardMedia component="img" height="250" image={kickersub} alt="kicker sub" />
+            <CardContent>
+              <Typography gutterBottom variant="h4" component="div">
+                Kicker
+              </Typography>
+              <Typography variant="body1" color="text.primary">
+                After installing for more than 5 years I have come to love Kicker for those wanting
+                to enter the car audio scene, as well as those who have been around for a while.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
         </Card>
         <Card sx={{ backgroundColor: 'gray' }}>
-          <CardMedia component="img" height="250" image={kickersub} alt="kicker sub" />
-          <CardContent>
-            <Typography gutterBottom variant="h4" component="div">
-              Kicker
-            </Typography>
-            <Typography variant="body1" color="text.primary">
-              After installing for more than 5 years I have come to love Kicker for those wanting to
-              enter the car audio scene, as well as those who have been around for a while.
-            </Typography>
-          </CardContent>
+          <CardActionArea>
+            <CardMedia component="img" height="250" image={kickersub} alt="kicker sub" />
+            <CardContent>
+              <Typography gutterBottom variant="h4" component="div">
+                Kicker
+              </Typography>
+              <Typography variant="body1" color="text.primary">
+                After installing for more than 5 years I have come to love Kicker for those wanting
+                to enter the car audio scene, as well as those who have been around for a while.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
         </Card>
       </div>
     </>
