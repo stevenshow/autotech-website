c86060c3eae6b53f2f81b686cd6b3a154450f574
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-08-01 16:26:47 -0600

    modal for more info

--- src/components/CardWrapper.js
+++ src/components/CardWrapper.js
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import { CardActionArea } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import InfoModal from '../components/InfoModal';
 import '../styles/CardWrapper.scss';
 
 const CardWrapper = ({ alt, title, image, children, minHeight = '250', objectFit }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
+      <InfoModal title={title} open={open} close={handleClose} />
       <Card>
-        <CardActionArea>
+        <CardActionArea onClick={handleOpen}>
           <CardMedia
             component="img"
             height={minHeight}
---
+++ src/components/InfoModal.js
@@ -0,0 +1,56 @@
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  borderRadius: '4px',
+  boxShadow: 24,
+  p: 4,
+};
+
+const InfoModal = ({ title, open, close }) => {
+  return (
+    <>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={close}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              {title}
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula. Duis mollis, est non
+              commodo luctus, nisi erat porttitor ligula. Duis mollis, est non commodo luctus, nisi
+              erat porttitor ligula. Duis mollis, est non commodo luctus, nisi erat porttitor
+              ligula. Duis mollis, est non commodo luctus, nisi erat porttitor ligula. Duis mollis,
+              est non commodo luctus, nisi erat porttitor ligula. Duis mollis, est non commodo
+              luctus, nisi erat porttitor ligula. Duis mollis, est non commodo luctus, nisi erat
+              porttitor ligula. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </>
+  );
+};
+
+export default InfoModal;
